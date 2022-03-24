dbeb74a3c25959877168ae6b37fe94c3e9e3747a
Elamir <engineeralamiir@gmail.com>
2022-03-24 11:33:44 +0200

    Upload Edits

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # mern-authentication-system
+
 a simple web app about user authentication, authorization, and protecting a specific page so that if you do not log in you can't navigate to it.
-using React, Node, Express, MongoDB, JWT and Bcrypt.
+using React, Node, Express, MongoDB, JWT and Bcrypt, Cookies
 
 Watch Through Youtube: https://www.youtube.com/watch?v=WrJEdH7nghY
+
 ```
 npm install
 npm run server
-npm run start 
+npm run start
 
 ```
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,13 +4,14 @@ const About = () => {
   return (
     <div className="about">
       <p>This simple app is about user authentication, authorization, and protecting a specific page so that if you do not log in you can't navigate to it.</p>
-      <p>Using React, Node, Express, MongoDB, JWT, Bcrypt</p>
+      <p>Using React, Node, Express, MongoDB, JWT, Bcrypt and Cookies</p>
       <div>
         <img src="https://img.icons8.com/plasticine/65/000000/react.png" />
         <img src="https://img.icons8.com/fluency/60/000000/node-js.png" />
         <img src="https://img.icons8.com/color/60/000000/mongodb.png" />
         <img src="https://img.icons8.com/color/60/000000/java-web-token.png" />
         <img src="https://img.icons8.com/flat-round/55/000000/lock--v1.png" />
+        <img src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/60/000000/external-cookie-christmas-flatart-icons-lineal-color-flatarticons.png" />
       </div>
     </div>
   );
